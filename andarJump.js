2b42daa841b800d68e9f2f9df3452a59b7bc174e
Tiffany Lam <tiffany.lam@triggerglobal.com>
2020-09-12 17:57:33 -0700

    First pass at the game

--- andarJump.js
+++ andarJump.js
@@ -28,39 +28,83 @@ var gameConfig = {
 }
 
 var game = new Phaser.Game(gameConfig);
+var main;
 var player;
-var level; 
+var level = 0; 
+var cabinetsSpawned = 0;
 var levelTimer;
 var timerTime;
 var cursors;
+var keyPress;
 var previousBackground;
+var isPaused;
+var currentVelocity = 150;
 
 function mainPreload(){
 	this.load.setBaseURL('https://raw.githubusercontent.com/chatterboxn18/chatterboxn18.github.io/master/')
+	this.load.image('background', 'andar-bg.png');
 	this.load.image('cabinet', 'andar-cabinet.png');
 	this.load.image('moonbyul', 'andar-moonbyul.png');
 	this.load.image('ground', 'andar-ground.png');
-	this.load.image('background', 'andar-bg.png');
 }
 
 function mainCreate(){
-	this.cameras.main.backgroundColor = Phaser.Display.Color.HexStringToColor("#f0c98e");
-	var cabinet = this.add.image(200,200, 'cabinet');
-	cabinet.setScale(.3);
+	main = this;
+	isPaused = false;
+	this.background = this.add.tileSprite(0,0, 1500, 500, "background")
+	this.background.setScale(.7);
+	this.background.setOrigin(0);
+	this.background.setScrollFactor(0,1);
+	cursors = this.input.keyboard.createCursorKeys();
+	keyPress = this.input.keyboard.addKey('P');
+
+	levelTimer = this.time.addEvent({
+		delay: 3000, 
+		callback: createCabinet, 
+		loop: true
+	})
 	createPlayer(this);
 	createGround(this);
-	this.background = this.add.tileSprite(0,0, 1500, 500, "background")
-		.setOrigin(0)
-		.setScrollFactor(0,1);
-	this.background.setTilePosition(this.cameras.main.scrollX);
+	createCabinet();
+}
 
+function createCabinet(){
+	if (cabinetsSpawned%5 == 0){
+		level += 1;
+		if (currentVelocity < 250){
+			currentVelocity += 10;
+		}
+		if (levelTimer.delay >2000){
+			levelTimer.delay -= 200;
+		}
+		console.log("Leveled up: " + currentVelocity + " "  + levelTimer.delay);
+	}
+	var cabinet = main.physics.add.sprite(530, 235, 'cabinet');
+	cabinet.setScale(.25);
+	cabinet.body.allowGravity = false;
+	cabinet.body.setVelocityX(-currentVelocity);
+	cabinet.body.onWorldBounds = true;
+	main.physics.add.collider(player, cabinet, touchObject, null, main);
+	var cabinetLifeTimer = main.time.delayedCall(5000, destroyCabinet, [cabinet]);
+	cabinetsSpawned++; 
+}
+
+function touchObject(player, object){
+	if (player.body.touching.right || player.body.touching.up || player.body.touching.left){
+		pause();
+		isPaused = true;
+		console.log("GameOver");
+	}
+}
+
+function destroyCabinet(cabinet){
+	cabinet.destroy(main);
 }
 
 function createPlayer(game){
 	player = game.physics.add.sprite(75, 0, 'moonbyul');
 	player.setScale(.3);
 	player.setCollideWorldBounds(true);
-	cursors = game.input.keyboard.createCursorKeys();
 }
 
 function createGround(game){
@@ -69,10 +113,35 @@ function createGround(game){
 	game.physics.add.collider(player, ground);
 }
 
+function pause(){
+	console.log("paused");
+	main.physics.pause();
+	levelTimer.paused = true;
+}
+
+function unpause(){
+	console.log("unpaused");
+	main.physics.resume();
+	levelTimer.paused = false;
+}
+
 function mainUpdate(){
+	if (keyPress.isDown){
+		isPaused = !isPaused;
+		if (isPaused)
+			pause();
+		else
+			unpause();
+	}
+
+	if (isPaused)
+		return;
+
+	this.background.tilePositionX += 5;
+
 	if (player.body.touching.down){
 		if (cursors.up.isDown){
-			player.setVelocityY(-220);
+			player.setVelocityY(-275);
 		}
 	}
 	else if (player.body.touching.down){
