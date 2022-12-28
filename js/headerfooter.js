01a52eacde42cdd6aadd4bb83108a5bf1790f85d
tiffany.lam <lambocreates@gmail.com>
2022-12-28 00:41:57 -0800

    Added all my personal projects

---
+++ js/headerfooter.js
@@ -0,0 +1,5 @@
+window.onload = function()
+{
+	var footer = document.getElementById('footer');
+	footer.innerHTML = "<footer class='tm-bg-gray pt-5 pb-3 tm-text-gray tm-footer'><div class=container-fluid tm-container-small'><div><ul class='tm-social-links d-flex justify-content-center pl-0 mb-5'><li class='mb-2'><a href='https://www.linkedin.com/in/tiffany-wai-ki-lam-137b80135/'><i class='fab fa-linkedin'></i></a></li><li class='mb-2'><a href='https://github.com/chatterboxn18'><i class='fab fa-github'></i></a></li></ul></div></div></footer>";
+}
